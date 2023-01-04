5071363e14bf5761ce0695ea42329171d3f96730
Jacob-32587 <jgbs32587@gmail.com>
2023-01-04 14:26:26 -0500

    First 6 Parts

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return { 
+      titleClass: 'title',
+      message: 'Hello World',
+      counter: {
+        count: 3
+      },
+      text1: '',
+      text2: '',
+      awesome: true
+    }
+  }, // Data
+  methods: {
+    increment() {
+      this.counter.count++; // Update count variable
+    },
+    onInput(e)
+    {
+      this.text1 = e.target.value;
+    },
+    toggle()
+    {
+      this.awesome = !this.awesome;
+    }
+  } // Methods
 }
 </script>
 
+<template>
+  <!-- Shorthand: ":class" -->
+  <h1 v-bind:class="titleClass"> {{ message }} </h1>
+   <!-- Shorthand: "@:click"  -->
+  <button v-on:click="increment">Count is: {{counter.count}}</button> 
+  <div></div>
+
+  <input :value="text1" @input="onInput" placeholder="Text...">
+  <p>{{ text1 }}</p>
+
+  <!-- V-model automaticlaly syncs the input's value to it's state so 
+  the onInput(e) methods is not needed anymore 
+  (also works with checkboxes, radio buttons, and select dropdowns.) -->
+  <input v-model="text2" placeholder="Text...">
+  <p>{{ text2 }}</p>
+
+  <button @click="toggle"> Toggle </button>
+  <h2 v-if="awesome">Vue is awesome!</h2>
+  <h2 v-else>:(</h2>
+
+</template>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.title {
+  color: red;
 }
 </style>
